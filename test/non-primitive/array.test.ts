997e350e83599d2768526722a4cd39afaadd881e
khangduy017 <khangduy017@gmail.com>
2023-12-20 17:07:58 +0700

    array type

---
+++ test/non-primitive/array.test.ts
@@ -0,0 +1,17 @@
+import VFT from "../..";
+
+// ===================== MAX LENGTH VALIDATION =====================
+const maxLengthValidator = VFT.array()
+  .maxLength(5)
+  .length(2)
+  .of(VFT.string().notEmpty());
+try {
+  const result1 = maxLengthValidator.validate(["1", "2", "3", "4"], {
+    stopOnFailure: false,
+  }); // expected true
+  console.log("Result of max length validator: ", result1);
+} catch (e) {
+  console.log(e);
+}
+
+// // const result1 = maxLengthValidator.validate('123456'); // expected error
