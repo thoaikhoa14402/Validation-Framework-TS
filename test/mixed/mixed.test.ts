4e98939c3a3902a002754386f911baf2847ff2b8
Khoa Nguyen <nguyenthoaidangkhoa@gmail.com>
2024-01-01 16:52:10 +0700

    feat: declare type constraint for custom validation (advanced feature)

--- test/mixed/mixed.test.ts
+++ test/mixed/mixed.test.ts
@@ -1,32 +1,68 @@
+import mongoose, { ObjectId } from "mongoose";
 import VFT from "../..";
 import { ValidationErrorContext } from "../../common/errors/error.ctx";
 
-const mixedValidator = VFT.mixed()
-  .addMethod("isEqual", (value: string, errCtx: ValidationErrorContext) =>
-    value === "test"
-      ? true
-      : errCtx!.createError({
-          message: "The string is not equal to test",
-          value: value,
-        })
-  )
-  .addMethod("isNotEqual", (value: string, errCtx: ValidationErrorContext) =>
-    value !== "test"
-      ? true
-      : errCtx!.createError({
-          message: "The string is equal to test",
-          value: value,
-        })
-  );
+// ===================== MIXED CUSTOM SCHEMA VALIDATION =====================
+// const mixedValidator = VFT.mixed()
+//   .addMethod("isEqual", (value: string, errCtx: ValidationErrorContext) =>
+//     value === "test"
+//       ? true
+//       : errCtx!.createError({
+//           message: "The string is not equal to test",
+//           value: value,
+//         })
+//   )
+//   .addMethod("isNotEqual", (value: string, errCtx: ValidationErrorContext) =>
+//     value !== "test"
+//       ? true
+//       : errCtx!.createError({
+//           message: "The string is equal to test",
+//           value: value,
+//         })
+//   );
 
-const chainValidator = mixedValidator.isEqual().isNotEqual();
+// const chainValidator = mixedValidator.isEqual().isNotEqual();
+
+// try {
+//   const result1 = chainValidator.validate("test", {
+//     stopOnFailure: false,
+//   });
+
+//   console.log("Result of mixed validator: ", result1);
+// } catch (err: any) {
+//   console.log("Error messages: ", err.message);
+//   console.log("Validation Errors: ", err.validationErrors);
+// }
+
+
+// ===================== MIXED CUSTOM SCHEMA VALIDATION WITH TYPE CONSTRAINT =====================
+const mixedValidator = VFT.mixed((input: unknown): input is ObjectId => input instanceof mongoose.Types.ObjectId)
+.addMethod("isEqual", (input: unknown, errCtx: ValidationErrorContext) =>
+      new mongoose.Types.ObjectId('507f1f77bcf86cd799439011').equals(input as any)
+      ? true
+      : errCtx!.createError({
+          message: "This ObjectId is not equal to ObjectId('507f1f77bcf86cd799439011')",
+          value: input,
+        })
+)
+
+const chainValidator = mixedValidator.isEqual();
 
 try {
-  const result1 = chainValidator.validate("test", {
-    stopOnFailure: false,
-  });
 
-  console.log("Result of mixed validator: ", result1);
+  const result2 = chainValidator.validate('@@@', {
+    stopOnFailure: false,
+  }); // expected error when checking with type constraint
+
+  // const result2 = chainValidator.validate(new mongoose.Types.ObjectId('507f1f77bcf86cd799439012'), {
+  //   stopOnFailure: false,
+  // }); // expected error when checking with isEqual method
+
+  // const result2 = chainValidator.validate(new mongoose.Types.ObjectId('507f1f77bcf86cd799439011'), {
+  //   stopOnFailure: false,
+  // }); // expected true
+
+  console.log("Result of mixed validator: ", result2);
 } catch (err: any) {
   console.log("Error messages: ", err.message);
   console.log("Validation Errors: ", err.validationErrors);
